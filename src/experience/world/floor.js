77fb6b770a577b1821ce81043778dc8ab2aae8e3
Francesco Dammacco <francescodammacco@live.it>
2025-01-26 15:28:13 +0100

    Axes helper tweak

--- src/experience/world/floor.js
+++ src/experience/world/floor.js
@@ -9,31 +9,34 @@ export default class Floor {
     this.experience = Experience.instance
     this.debug = Debug.instance.addFolder('floor')
 
+    this.size = 20
     this.scene = this.experience.scene
 
-    this.size = 20
-    this.create()
-
     //prettier-ignore
-    this.debug.add(this, 'size').min(10).max(100).step(1).onChange(() => {
+    this.debug.add(this, 'size').min(10).max(50).step(1).onChange(() => {
       this.destroy()
       this.create()
     })
+
+    this.create()
   }
 
   create() {
     this.gridHelper = new GridHelper(this.size, this.size * (1 / 0.5))
 
     this.axesHelper = new AxesHelper(this.size / 2)
-    this.axesHelper
+    this.axesHelper.visible = Debug.active
     this.axesHelper.position.set(0.001, 0.001, 0.001)
 
     this.scene.add(this.gridHelper, this.axesHelper)
+
+    this.debug.add(this.axesHelper, 'visible').name('axesHelper')
   }
 
   destroy() {
     dispose(this.gridHelper)
     dispose(this.axesHelper)
+    this.debug.controllers.find(c => c._name === 'axesHelper')?.destroy()
     this.scene.remove(this.gridHelper, this.axesHelper)
   }
 }
