0632620978b3d5880400477f82d50981490c105a
Francesco Dammacco <francescodammacco@live.it>
2025-02-21 17:27:51 +0100

    Add standalone padding bottom

--- src/script.js
+++ src/script.js
@@ -2,3 +2,10 @@ import Experience from './experience/experience'
 
 Experience.init(document.querySelector('canvas.webgl'))
 document.querySelectorAll('.fab').forEach(b => b.addEventListener('focus', e => e.target.blur()))
+
+const isStandalone = () =>
+  window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone
+
+if (isStandalone()) {
+  document.body.classList.add('standalone')
+}
